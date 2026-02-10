b36f01cd79c0e706b2457095dc62fa2231c835af
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2026-02-10 16:23:55 +0530

    Create ddl_bronze.sql

---
+++ Script/bronze/ddl_bronze.sql
@@ -0,0 +1,63 @@
+/*
+
+============================================
+Create database and schemas
+============================================
+
+Script purpose:
+This script creates a new database named 'SecurityLogsDW' after checking if it already exists.
+If the database exists, it drops and recreated. Additionally, the script sets up the three schemas
+within the database: bronze, silver, and gold.
+
+Warning:
+Running this script will drop the entire data warehouse database if it exists. 
+All data in the database will permanently be deleted. Proceed with caution 
+and ensure you have proper backups before running this script 
+
+*/
+
+
+USE master;
+ 
+ 
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'SecurityLogsDW')
+BEGIN
+  ALTER DATABASE SecurityLogsDW SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE SecurityLogsDW;
+END;
+
+create database SecurityLogsDW;
+USE SecurityLogsDW;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
+if object_id('bronze.login_attempts','U') is not null
+drop table bronze.login_attempts;
+go
+
+CREATE TABLE bronze.login_attempts (
+   
+    login_timestamp     VARCHAR(max),
+    rtt_ms              VARCHAR(max),
+    ip_address          VARCHAR(max),
+    country             VARCHAR(max),
+    region              VARCHAR(max),
+    city                VARCHAR(100),
+    asn                 VARCHAR(100),
+    browser_name        VARCHAR(MAX),
+    os_name             VARCHAR(MAX),
+    device_type         VARCHAR(100),
+    login_successful    VARCHAR(50),
+    is_attack_ip        VARCHAR(50),
+    is_account_takeover VARCHAR(50)
+);
+
+
