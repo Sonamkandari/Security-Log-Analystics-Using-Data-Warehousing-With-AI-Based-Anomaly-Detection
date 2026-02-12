555be3198cf169745de7e3a031f776cbc65864cb
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2026-02-12 12:45:07 +0530

    Create login_attempts table in silver schema
    This script creates the 'login_attempts' table in the 'silver' schema, dropping it if it already exists.

--- Script/silver/ddl_silver.sql
+++ Script/silver/ddl_silver.sql
@@ -1,1 +1,40 @@
 
+
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+DDL script: Create Silver Tables
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Script Purpose:
+     This script creates tables in the 'silver' schema,dropping existing tables 
+     if they already exist.
+     Run this script to re-define the DDL structure of 'bronze' Tables
+=============================================================================== 
+*/
+
+use SecurityLogsDW;
+
+
+IF OBJECT_ID('silver.login_attempts', 'U') IS NOT NULL
+    DROP TABLE silver.login_attempts;
+GO
+
+CREATE TABLE silver.login_attempts (
+    login_time           TIME,
+    rtt_ms               INT,
+    ip_address           VARCHAR(45),
+    country              VARCHAR(10),
+    region               VARCHAR(100),
+    city                 VARCHAR(100),
+    asn                  VARCHAR(50),
+    browser_name         VARCHAR(100),
+    browser_family VARCHAR(100),
+    browser_version VARCHAR(100),
+    os_name              VARCHAR(100),
+    device_type          VARCHAR(50),
+    login_successful     BIT,
+    is_attack_ip         BIT,
+    is_account_takeover  BIT
+);
+GO
+
+
