f623e034cfb61414a37cda81d93dcb9fb4960ae0
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2026-02-17 16:28:22 +0530

    Create intrusion_detection table in silver schema
    Added DDL for intrusion_detection table with relevant fields.

--- Script/silver/ddl_silver.sql
+++ Script/silver/ddl_silver.sql
@@ -1,5 +1,4 @@
 
-
 /*
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 DDL script: Create Silver Tables
@@ -36,5 +35,26 @@ CREATE TABLE silver.login_attempts (
     is_account_takeover  BIT
 );
 GO
+
+IF OBJECT_ID('silver.intrusion_detection', 'U') IS NOT NULL
+    DROP TABLE silver.intrusion_detection;
+GO
+
+CREATE TABLE silver.intrusion_detection (
+    session_id            VARCHAR(50),        -- Session identifier (SID_00001)
+    network_packet        INT,                -- Packet count / metric
+    protocol_type         VARCHAR(10),         -- TCP / UDP / ICMP
+    login_attempts        INT,                -- Number of login attempts
+    session_duration      DECIMAL(18,6),       -- Session duration
+    encryption_used       VARCHAR(20),         -- AES / DES / NULL
+    ip_reputation_score   DECIMAL(18,6),       -- Risk score (0–1)
+    failed_logins         INT,                -- Failed login count
+    browser_type          VARCHAR(50),         -- Chrome / Firefox / Edge
+    unusual_time_access   BIT,                -- Boolean flag
+    attack_detected       BIT,                -- Ground-truth attack label
+    source_system         VARCHAR(50)          -- Data lineage
+);
+GO
+
 
 
